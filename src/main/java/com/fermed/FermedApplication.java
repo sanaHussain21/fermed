aa9058e3977ae1e46546b589c38909bf49cfe8c8
Sana Hussain <hussainsana687@gmail.com>
2023-02-27 16:12:56 +0100

    INITIAL COMMIT V1

---
+++ src/main/java/com/fermed/FermedApplication.java
@@ -0,0 +1,13 @@
+package com.fermed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FermedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FermedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fermed/FermedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fermed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FermedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

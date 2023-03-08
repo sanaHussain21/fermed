8acd6122926b5683d237aa80668515fd727b917a
Sana Hussain <hussainsana687@gmail.com>
2023-03-08 20:54:08 +0100

    starting working on dao class

--- src/main/java/com/fermed/DAO/DoctorDAO.java
+++ src/main/java/com/fermed/DAO/DoctorDAO.java
@@ -1,5 +1,26 @@
 package com.fermed.DAO;
 
-public interface DoctorDAO {
+import org.springframework.boot.autoconfigure.cassandra.CassandraProperties;
+
+import java.sql.Connection;
+
+public class DoctorDAO {
     //DoctorDAO will take the data from the database
+    
+    //creating connectioon with the database
+    private static Connection connection = null;
+
+    //return connection
+    public static Connection getConnection(){
+        if(connection != null){
+            return connection;
+        }else {
+            String driver = "com.mysql.cj.jdbc.Driver";
+            String url = "jdbc:mysql://localhost:3306/fermed?serverTimezone=UTC";
+        }
+        return connection;
+    }
+
+
+
 }

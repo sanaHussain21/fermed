8c6d367305d066b9e84bc38ab8785efe9ed70c16
Sana Hussain <hussainsana687@gmail.com>
2023-03-10 21:56:39 +0100

    INITAL COMMIT V1

---
+++ src/main/java/com/fermed/DTO/ApiResponse.java
@@ -0,0 +1,12 @@
+package com.fermed.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiResponse {
+    private String message;
+    private boolean success;
+
+}
--- src/main/java/com/fermed/controllers/HomeController.java
+++ src/main/java/com/fermed/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.fermed.controllers;
 
+import com.fermed.DTO.ApiResponse;
 import com.fermed.DTO.DoctorDTO;
 import com.fermed.facades.DoctorFacade;
 import com.fermed.model.Doctor;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.print.Doc;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -29,9 +31,9 @@ public class HomeController {
     //Spring boot REST API
 
     //POST CREATING DOCTOR
-    @PostMapping("/")
+    @PostMapping("/createDoctor")
         public ResponseEntity<DoctorDTO> createDoctor(@RequestBody DoctorDTO doctorDTO){
-        DoctorDTO savedDoctor = doctorFacade.createDoctor(doctorDTO);
+        DoctorDTO savedDoctor = this.doctorService.createDoctor(doctorDTO);
         return new ResponseEntity<>(savedDoctor, HttpStatus.CREATED);
 
 
@@ -40,15 +42,36 @@ public class HomeController {
     //Spring Boot REST API a Doctor
     @GetMapping("/doctorById")
     public ResponseEntity<DoctorDTO> getDoctorById(@PathVariable("") Integer id_doctor){
-        DoctorDTO doctor = doctorFacade.getDoctorById(id_doctor); //talking with facade
+        DoctorDTO doctor = this.doctorFacade.getDoctorById(id_doctor); //talking with facade
             return new ResponseEntity<>(doctor, HttpStatus.OK);
     }
 
         //creating API to return  the doctor list
-        @GetMapping("/doctorList")
-        public List<Doctor> getAllDoctors(){
-        return this.doctorService.getAllDoctors();
+        @GetMapping("/")
+        public ResponseEntity<List<Doctor>> getAllDoctors(){
+        return ResponseEntity.ok(this.doctorService.getAllDoctors());
         }
+
+        //PUT - update doctor
+        @PutMapping("/{doctorId}")
+        public ResponseEntity<DoctorDTO> updateDoctor(@RequestBody DoctorDTO doctorDTO, @PathVariable("doctorId") Integer doctorId )
+        {
+          DoctorDTO updatedDoctor =  this.doctorService.updateDoctor(doctorDTO, doctorId);
+          return ResponseEntity.ok(updatedDoctor);
+        }
+
+        //DELETE - DELETE DOCTOR
+    @DeleteMapping("/{doctorId}")
+        public ResponseEntity<ApiResponse> deleteDoctor(@PathVariable("doctorId") Integer doctorId)
+        {
+            ResponseEntity<?> deleteDoctor = this.deleteDoctor(doctorId);
+            return new ResponseEntity<ApiResponse>(new ApiResponse("Doctor deleted Successfully", true), HttpStatus.OK);
+        }
+
+
+
+
+
 
     }
 

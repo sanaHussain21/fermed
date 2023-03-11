8a085418a3058b48bf1a8d7f2ecc36bfb840df8b
Sana Hussain <hussainsana687@gmail.com>
2023-03-11 14:35:37 +0100

    created exception package and GlobalExceptionHandler class

--- src/main/java/com/fermed/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fermed/exception/GlobalExceptionHandler.java
@@ -1,8 +1,24 @@
 package com.fermed.exception;
 
+import com.fermed.DTO.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     //this class will handle the particular exception when it will raise
+    //with the help of this annotation  @ControllerAdvice
+    //it will scan all controllers
+
+
+    //ResourceNotFoundException this is custom exception
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> resourceNotFoundException(ResourceNotFoundException exception){
+        String message = exception.getMessage();
+        ApiResponse apiResponse = new ApiResponse(message, false);
+        return new ResponseEntity<ApiResponse>(apiResponse,  HttpStatus.NOT_FOUND);
+    }
 }

4316f5ede6e81309ce9d68f91cc82540329075ab
Sana Hussain <hussainsana687@gmail.com>
2023-03-17 16:19:34 +0100

    modified homefaced clas

--- src/main/java/com/fermed/facades/HomeFacade.java
+++ src/main/java/com/fermed/facades/HomeFacade.java
@@ -1,27 +1,17 @@
 package com.fermed.facades;
 
 import com.fermed.model.Doctor;
-import com.fermed.services.HomeService;
-import lombok.Data;
-
-import javax.annotation.Resource;
 import java.util.List;
-import java.util.Optional;
+
 
 
 public interface HomeFacade{
 
-   //should be written the list doctor method signature
-   @Resource
-   private HomeService homeService;
+   //getting list of doctors
+   List<Doctor> getAllDoctors();
 
 
 
-   @Override
-   public List<Doctor> getAllDoctors() {
-      System.out.println("Siamo passati da controllr through facade through a service");
-      return this.homeService.getAllDoctors();
-   }
 
 
 }

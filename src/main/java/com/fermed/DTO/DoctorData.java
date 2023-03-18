9d06c6bd1b32e8864a096325d5e9d376115e4ce8
Sana Hussain <hussainsana687@gmail.com>
2023-03-18 12:20:37 +0100

    modified class named doctorData

--- src/main/java/com/fermed/DTO/DoctorData.java
+++ src/main/java/com/fermed/DTO/DoctorData.java
@@ -1,4 +1,11 @@
 package com.fermed.DTO;
 
 public class DoctorData {
+    private int id_doctor;
+    private String name;
+    private String surname;
+    private String gender;
+    private String email;
+    private String password;
+    private String username;
 }

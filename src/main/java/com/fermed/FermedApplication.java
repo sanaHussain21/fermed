70c386f873a83cc94ee8f91ecccc7e3928749e9b
Sana Hussain <hussainsana687@gmail.com>
2023-03-26 15:13:04 +0200

    modified and added some new files

--- src/main/java/com/fermed/FermedApplication.java
+++ src/main/java/com/fermed/FermedApplication.java
@@ -5,13 +5,14 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Data
 
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
 
 public class FermedApplication {
 
--- src/main/java/com/fermed/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/fermed/SecurityConfig/SecurityConfig.java
@@ -1,4 +1,18 @@
 package com.fermed.SecurityConfig;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
 public class SecurityConfig {
+    //this is needed to encode the password
+/*
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+*/
+
 }

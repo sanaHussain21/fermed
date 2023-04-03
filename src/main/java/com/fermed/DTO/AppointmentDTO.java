35d8e6658b4c57523b3f952b75c8b8f9c08fec5c
Sana Hussain <hussainsana687@gmail.com>
2023-04-03 20:30:54 +0200

    modified some appointmentDTO CLASS

--- src/main/java/com/fermed/DTO/AppointmentDTO.java
+++ src/main/java/com/fermed/DTO/AppointmentDTO.java
@@ -2,16 +2,26 @@ package com.fermed.DTO;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.util.Date;
 
 @Data
 public class AppointmentDTO {
 
     private int id_appointment;
+
+    @NotEmpty
+    @NotBlank(message = "Must select a date!!!")
     private Date time_date;
+
+    //chiedere
     private int payment;
+
     private int patient_id;
     private  int ID_DOC;
+
+    
     private boolean IsBeingNotified;
     private boolean NotifiedByEmail;
     private  boolean NotifiedBySMS;

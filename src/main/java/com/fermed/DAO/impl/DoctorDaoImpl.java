25b6a8fb58886723729c20c22f7f6b98aac3c917
Sana Hussain <hussainsana687@gmail.com>
2023-04-16 20:59:32 +0200

    modfied sme files

--- src/main/java/com/fermed/DAO/impl/DoctorDaoImpl.java
+++ src/main/java/com/fermed/DAO/impl/DoctorDaoImpl.java
@@ -97,11 +97,4 @@ public class DoctorDaoImpl implements DoctorDAO {
 
     }
 
-        try {
-                PreparedStatement preparedStatement = connection.prepareStatement("SELECT name, surname, gender, username ,email  FROM doctor WHERE email = '"+doctorDTO.getEmail()+"'");
-                ResultSet resultSet = preparedStatement.executeQuery();
-                while(resultSet.next()){
-                //for testing
-                System.out.println(resultSet.getString(1) + ",   " + resultSet.getString(2)+ ",   " + resultSet.getString(3)+ ",   " + resultSet.getString(4)+ ",   "+ resultSet.getString(5)+ "   ");
-
-                }
+    

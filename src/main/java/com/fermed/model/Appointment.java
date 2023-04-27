c38e0bdf622ae608e1d2713d9a0ce65594511d11
Sana Hussain <hussainsana687@gmail.com>
2023-04-27 12:00:21 +0200

    added new entity called appointment.java

--- src/main/java/com/fermed/model/Appointment.java
+++ src/main/java/com/fermed/model/Appointment.java
@@ -3,9 +3,23 @@ package com.fermed.model;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @Entity
 @Data
 public class Appointment {
+    //appointment entity, using lombol
+
+    int id_appuntamento;
+     Date time_date;
+     int payment;
+     int patient_id;
+     int ID_DOC;
+     boolean IsBeingNotified;
+     boolean NotifiedByEmail;
+     boolean NotifiedBySMS;
+
+
 
 }

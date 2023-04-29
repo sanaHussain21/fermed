c59cd8fa2d3c8566e355d7859411b740016a7912
Sana Hussain <hussainsana687@gmail.com>
2023-04-29 22:21:36 +0200

    modified some files

--- src/main/java/com/fermed/model/Appointment.java
+++ src/main/java/com/fermed/model/Appointment.java
@@ -35,12 +35,7 @@ public class Appointment {
 }
 
 
-    int payment;
-    int patient_id;
-    int ID_DOC;
-    boolean IsBeingNotified;
-    boolean NotifiedByEmail;
-    boolean NotifiedBySMS;
+    
 
 
 

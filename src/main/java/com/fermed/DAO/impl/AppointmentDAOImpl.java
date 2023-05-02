5b402bdc8223e1f9d7ee34d9ed3b327610829578
Sana Hussain <hussainsana687@gmail.com>
2023-05-02 10:49:08 +0200

    modified some files

--- src/main/java/com/fermed/DAO/impl/AppointmentDAOImpl.java
+++ src/main/java/com/fermed/DAO/impl/AppointmentDAOImpl.java
@@ -28,10 +28,20 @@ public class AppointmentDAOImpl implements AppointmentDAO {
         String insertQuery = "INSERT INTO appuntamento(time_date, payment, patient_id , ID_DOC , IsBeingNotified, NotifiedByEmail, NotifiedBySMS)" +
                 //"VALUES('2', '2023-05-23 13:30:00', '30', '55' , '39', true, true, false)";
                 "VALUES('" + appointment.getTime_date() + "', '" + appointment.getPayment() + "', " + appointment.getPatient_id() + ",  " + appointment.getId_doc() + ", " + appointment.isBeingNotified() + ",  " + appointment.isNotifiedByEmail() + " , " + appointment.isNotifiedBySMS() + ")";
+
+
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
             preparedStatement.executeUpdate();
             System.out.println("APPOINTMENT CREATED SUCCESSFULLY! :)");
+            //for testing:
+
+
+
+
+
+
+
         } catch (SQLException e) {
             e.printStackTrace();
         }

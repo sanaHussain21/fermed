70bf55e99a45a1cb28a104eda2d603257a63cc11
Sana Hussain <hussainsana687@gmail.com>
2023-05-12 13:39:54 +0200

    modified some file

--- src/main/java/com/fermed/controllers/AppointmentController.java
+++ src/main/java/com/fermed/controllers/AppointmentController.java
@@ -2,6 +2,7 @@ package com.fermed.controllers;
 
 import com.fermed.DTO.AppointmentDTO;
 import com.fermed.facades.AppointmentFacade;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -24,9 +25,12 @@ public class AppointmentController {
 
 
     //this post mapping is needed to create the appointment
+    // @PostMapping(path = "/createAppointment", consumes = MediaType.APPLICATION_JSON_VALUE)
     @PostMapping(path = "/createAppointment")
 
     public void createAppointment(@Valid @RequestBody AppointmentDTO appointmentDTO) throws Exception {
+
+
         appointmentFacade.createAppointment(appointmentDTO);
         //HttpSession session  = request.getSession();
         //session.setAttribute("patientName", appointmentDTO);

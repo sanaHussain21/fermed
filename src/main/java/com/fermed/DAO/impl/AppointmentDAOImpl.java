65009bddceea31357b88a4da1b3bc5aacf3995b2
Sana Hussain <hussainsana687@gmail.com>
2023-05-18 16:36:21 +0200

    modifief some file

--- src/main/java/com/fermed/DAO/impl/AppointmentDAOImpl.java
+++ src/main/java/com/fermed/DAO/impl/AppointmentDAOImpl.java
@@ -60,7 +60,7 @@ public class AppointmentDAOImpl implements AppointmentDAO {
 
     //getting appointmrnt data
 
-   private List<AppointmentDTO> appointmentList = new ArrayList<>() ;
+   List<AppointmentDTO> appointmentList = new ArrayList<>() ;
 
 
     @Override

c26db939352f27ae817eecb8baea7c52c2617b07
Sana Hussain <hussainsana687@gmail.com>
2023-05-21 16:03:54 +0200

    modified some file

--- src/main/java/com/fermed/repository/AppointmentRepository.java
+++ src/main/java/com/fermed/repository/AppointmentRepository.java
@@ -1,4 +1,12 @@
 package com.fermed.repository;
 
-public interface AppointmentRepository {
+import com.fermed.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+
+@EnableJpaRepositories
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
 }

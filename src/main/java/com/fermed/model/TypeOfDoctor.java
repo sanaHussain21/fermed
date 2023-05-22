1e0ec3c37643c8a6610572e04eb2e2d936d53e6a
Sana Hussain <hussainsana687@gmail.com>
2023-05-22 18:48:31 +0200

    modified some file

--- src/main/java/com/fermed/model/TypeOfDoctor.java
+++ src/main/java/com/fermed/model/TypeOfDoctor.java
@@ -1,4 +1,14 @@
 package com.fermed.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity
 public class TypeOfDoctor {
+   int id_type_of_doctor;
+   String nome;
+    int  code;
+   String  description;
 }

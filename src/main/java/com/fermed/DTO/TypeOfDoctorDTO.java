7789e3d5e3826e959662c88c70adbda58e0e0b5b
Sana Hussain <hussainsana687@gmail.com>
2023-05-23 10:10:31 +0200

    modified some file

--- src/main/java/com/fermed/DTO/TypeOfDoctorDTO.java
+++ src/main/java/com/fermed/DTO/TypeOfDoctorDTO.java
@@ -1,4 +1,28 @@
 package com.fermed.DTO;
 
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Data
 public class TypeOfDoctorDTO {
+    //Using doctorDTO for transfering the data
+
+    int id_type_of_doctor;
+
+    @NotEmpty
+    @NotBlank(message = "Doctor Department field should not be empty!!!")
+    String nome;
+
+    @NotEmpty
+    @NotBlank(message = "Code field should not be empty!!!")
+    int  code;
+
+    @NotEmpty
+    @NotBlank(message = "Description field should not be empty!!!")
+    @Size(min = 20, message ="Description must contain minimum 20 characters!!!")
+    @Size(max = 50, message ="Description must contain maximum 50 characters!!!" )
+    String  description;
 }

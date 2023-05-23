aa887b833cdce2051e14d0a457a3a5b903912d91
Sana Hussain <hussainsana687@gmail.com>
2023-05-23 10:00:59 +0200

    modified some file

--- src/main/java/com/fermed/controllers/TypeOfDoctorController.java
+++ src/main/java/com/fermed/controllers/TypeOfDoctorController.java
@@ -3,10 +3,12 @@ package com.fermed.controllers;
 import com.fermed.facades.TypeOfDoctorFacade;
 import com.fermed.model.TypeOfDoctor;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/typeOfDoctor")
@@ -18,6 +20,8 @@ public class TypeOfDoctorController {
     private TypeOfDoctorFacade typeOfDoctorFacade;
 
     //creating API to return doctor type list
+
+    @GetMapping("/typeOfDoctor")
 
 
 

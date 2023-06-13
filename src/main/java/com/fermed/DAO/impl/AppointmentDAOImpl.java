11ebfdff3c736e5dcb448384a30f49ceae40e568
Sana Hussain <hussainsana687@gmail.com>
2023-06-13 21:38:50 +0200

    modified some file

--- src/main/java/com/fermed/DAO/impl/AppointmentDAOImpl.java
+++ src/main/java/com/fermed/DAO/impl/AppointmentDAOImpl.java
@@ -294,13 +294,7 @@ return  appointment1;
     }
 }
 
- try {
-         PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
-         preparedStatement.executeUpdate();
-         System.out.println("APPOINTMENT CREATED SUCCESSFULLY! :)");
-         } catch (SQLException e) {
-         e.printStackTrace();
-         }
+
 
 
 

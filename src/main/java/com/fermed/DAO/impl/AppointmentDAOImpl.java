0d26f7cf378f65b232bfc9597845b79ab8761320
Sana Hussain <hussainsana687@gmail.com>
2023-06-13 21:33:25 +0200

    modified soem file+

--- src/main/java/com/fermed/DAO/impl/AppointmentDAOImpl.java
+++ src/main/java/com/fermed/DAO/impl/AppointmentDAOImpl.java
@@ -296,9 +296,15 @@ return  appointment1;
 
 
 
+ try {
+         PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+         preparedStatement.executeUpdate();
+         System.out.println("APPOINTMENT CREATED SUCCESSFULLY! :)");
+         } catch (SQLException e) {
+         e.printStackTrace();
+         }
 
 
- 
 
 
 
